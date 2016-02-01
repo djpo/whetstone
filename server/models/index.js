c7b97b41001a113a07956899ff9388d9cef7a590
rk23 <reed.kinning@gmail.com>
2016-02-01 13:31:42 -0800

    init

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  //Add models here
+  //Example
+  //user : require('./User')
+
+
+};
---
+++ server/routes.js
@@ -0,0 +1,10 @@
+var path = require('path');
+module.exports = {
+
+  '/' : function(req, res){
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+  },
+  '/example' : require('./controllers/delete-this')
+
+
+};
