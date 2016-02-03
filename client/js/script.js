da2bc4db2946f8cbd2f98e1993da7edb58328edd
djpo <derekpoppe@gmail.com>
2016-02-02 18:56:32 -0800

    create_goal.ejs mostly populated.

--- client/js/script.js
+++ client/js/script.js
@@ -1,6 +1,9 @@
-$(function(){
+// $(function(){
+// })
 
-  
+$(document).ready(function() {
 
+		// for Materialize 'select' input type (dropdown menu)
+    $('select').material_select();
 
-})
+});
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var express     = require('express'),
-    mongoose    = require('mongoose'),
-    request     = require('request'),
-    path        = require('path'),
-    session     = require('express-session'),
-    bodyParser  = require('body-parser'),
-    ejsLayouts  = require("express-ejs-layouts"),
-    passport    = require('passport'),
-    db          = require('./server/models/index.js'),
-    _           = require('lodash'),
+var express       = require('express'),
+    mongoose      = require('mongoose'),
+    request       = require('request'),
+    path          = require('path'),
+    session       = require('express-session'),
+    bodyParser    = require('body-parser'),
+    ejsLayouts    = require("express-ejs-layouts"),
+    passport      = require('passport'),
+    db            = require('./server/models/index.js'),
+    _             = require('lodash'),
     LocalStrategy = require('passport-local').Strategy;
 
-var app         = express();
+var app           = express();
 
 // Middleware
 app.set('view engine', 'ejs');
