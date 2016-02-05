46afe96f798f5366925f14a177b768657ec1391f
djpo <derekpoppe@gmail.com>
2016-02-04 21:32:14 -0800

    Made a dropdown nav menu.

--- client/js/script.js
+++ client/js/script.js
@@ -1,17 +1,19 @@
-// $(function(){
-// })
-
 $(document).ready(function() {
 
-		// for Materialize 'select' input type (dropdown menu)
-    $('select').material_select();
-    $('.modal-trigger').leanModal({
-          dismissible: true, // Modal can be dismissed by clicking outside of the modal
-          opacity: .6, // Opacity of modal background
-          in_duration: 150, // Transition in duration
-          out_duration: 100, // Transition out duration
-      }
-    );
+  // for Materialize modal
+  $('.modal-trigger').leanModal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .6, // Opacity of modal background
+    in_duration: 100, // Transition in duration
+    out_duration: 100, // Transition out duration
+  });
 
+  // for Materialize dropdown menu button
+  $('.dropdown-button').dropdown({
+     belowOrigin: true,
+     constrain_width: true,
+     inDuration: 100,
+     outDuration: 100
+   });
 
 });
