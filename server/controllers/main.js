ee96b8d6e3e0574aa75cd10626998d2bcebbe9a5
rk23 <reed.kinning@gmail.com>
2016-02-08 10:54:06 -0800

    name vs username on mainjs

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -70,7 +70,7 @@ router.get('/dashboard', function(req, res){
         var counter = 0; // Need an external counter because i is asynchronous, may go 0, 2, 1 3 instead of 0, 1, 2, 3
         goal.members.forEach(function(member, i, array){
           db.user.findOne({_id: member}, function(err, user){
-            var name = user.username.split('@')[0]
+            var name = user.name
             friendStatus.push([name, user.currentGoals[goal.id].submitted_today])
             counter++;
             if(counter === array.length){
